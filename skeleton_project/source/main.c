52410ac8e6085f4e34f7d3df0df6c66ecd06c1f4
Sherine7734 <sherines@stud.ntnu.no>
2026-02-24 11:43:52 +0100

    Add files via upload

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <time.h>
-#include "driver/elevio.h"
+//#include "driver/elevio.h"
+#include "driver/floor_light.h"
+#include "driver/stop_button.h"
+#include "driver/door.h"
+#include "driver/motor.h"
+#include "driver/start_up.h"
 
 
 
@@ -12,10 +17,22 @@ int main(){
     printf("=== Example Program ===\n");
     printf("Press the stop button on the elevator panel to exit\n");
 
-    elevio_motorDirection(DIRN_UP);
+    //elevio_motorDirection(DIRN_UP);
+    int floor = elevio_floorSensor();
+    start_up(floor);//HELP!!!!!!!!! How do we get the elevator to start going down?????
+
 
     while(1){
-        int floor = elevio_floorSensor();
+        printf("%d\n", floor);
+        floor = elevio_floorSensor();
+        setFloorLight(floor); //*
+        activate_stop_light(); //*
+        open_door_when_stop(floor); //*
+        keep_door_open_when_not_stop(floor);//*
+        door_state_implementation();//*
+        door_obstruction();//*
+        //printf("%s\n", "StopButton:");//*
+        //printf("%d\n", elevio_stopButton()); //*
 
         if(floor == 0){
             elevio_motorDirection(DIRN_UP);
@@ -41,7 +58,8 @@ int main(){
         
         if(elevio_stopButton()){
             elevio_motorDirection(DIRN_STOP);
-            break;
+            activate_stop_light();
+            //break;
         }
         
         nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
